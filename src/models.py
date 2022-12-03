2a246e8eb56bbd5710b51b6b5a913d6b89bb13d2
plopezrodrigo <patrylr84@gmail.com>
2022-12-03 17:49:11 +0000

    Mi primer commit

--- src/models.py
+++ src/models.py
@@ -8,23 +8,43 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
     name = Column(String(250), nullable=False)
+    phone = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    date = Column(Integer, nullable=False)
+    image = Column(String(250), nullable=False)
+    title = Column(String(250), nullable=False)
+    content = Column(String(250), nullable=False)
+
+class like(Base):
+    __tablename__ = 'like'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    id_post = Column(Integer, ForeignKey('post.id'))
+
+class comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    id_post = Column(Integer, ForeignKey('post.id'))
+    texto = Column(String(250), nullable=False)
 
     def to_dict(self):
         return {}
@@ -36,3 +56,4 @@ try:
 except Exception as e:
     print("There was a problem genering the diagram")
     raise e
+
